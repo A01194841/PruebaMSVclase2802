3301c7748336fe3d14fd79d90e7fbf2797443c15
Mariano Sistos vallin <A01194841@tec.mx>
2023-02-28 14:45:43 -0600

    Clase 1 Demanda cambio 1

--- PruebaMSVclase2802/Algoritmo/Clase/Demanda.cs
+++ PruebaMSVclase2802/Algoritmo/Clase/Demanda.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace PruebaMSVclase2802.Algoritmo.Clase
 {
-    internal class Demanda
+    public class Demanda
     {
+        public string IdProducto { get; set; }
+        public string IdCliente { get; set; }
+        public double Cantidad { get; set; }
+        public Demanda ()
+        {
+        }
+        public Demanda(string idProducto, string idCliente, double cantidad)
+        {
+            IdProducto = idProducto;
+            IdCliente = idCliente;
+            Cantidad = cantidad;
+        }
     }
 }
