68176e50d3055722428ce240528491f5af48c81a
Mariano Sistos vallin <A01194841@tec.mx>
2023-02-28 14:40:57 -0600

    Agregar archivos de proyecto.

---
+++ PruebaMSVclase2802/Algoritmo/Clase/Demanda.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaMSVclase2802.Algoritmo.Clase
+{
+    internal class Demanda
+    {
+    }
+}
